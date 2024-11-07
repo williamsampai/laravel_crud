107336964a4b6db87870d86be076aa6f3b617b7e
williamsampai <martinswillammartinssampaio@gmail.com>
2024-11-07 14:46:37 -0300

    first commit

---
+++ crud_reservas/resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Document</title>
+    <style>
+        #banner{
+
+        }
+
+    </style>
+</head>
+<body>
+    @section('content')
+        @include('layouts.navigation')
+
+        <img id="banner"   src="{{asset('images/reserva.png')}}" alt="" style="padding-top: 62px; height:100vh; width:100%">
+</body>
+</html>
